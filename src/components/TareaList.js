b54cac076425f77405bfe9d9da9c09c190fb6495
Fedreric <116675592+Fedreric@users.noreply.github.com>
2023-04-24 17:19:29 -0300

    Se agrega la logica necesaria para editar las tareas

--- src/components/TareaList.js
+++ src/components/TareaList.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import TareaItem from './TareaItem'
 
-export default function TareaList({tareas, onCompletado, onBorrarItem}) {
+export default function TareaList({tareas, onCompletado, onBorrarItem, onEditarItem}) {
   return (
     <div>
         {
             tareas.map((tarea, index) =>(
                 <TareaItem key = {`tarea-${index}`} tarea = {tarea} onCompletado = {onCompletado}
-                onBorrarItem = {onBorrarItem}/>
+                onBorrarItem = {onBorrarItem} onEditarItem = {onEditarItem}/>
             ))
         }
     </div>
